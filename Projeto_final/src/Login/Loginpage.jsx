8c5392b250ef9a24ccc089093f3e1e60cfd258e6
jlfernandes22 <95943327+jlfernandes22@users.noreply.github.com>
2024-11-15 10:21:18 +0000

    Update Loginpage.jsx

--- Projeto_final/src/Login/Loginpage.jsx
+++ Projeto_final/src/Login/Loginpage.jsx
@@ -1,7 +1,6 @@
 import './loginpage.css';
 import React, { useState } from 'react';
 import SignInForm from './SignInForm.jsx';
-import SignIn from './SignIn.css';
 
 function Loginpage() {
     const [formToShow, setFormToShow] = useState(null);
@@ -15,10 +14,10 @@ function Loginpage() {
             <div className="login_buttons">
                 <h1 className="heading_1">WHATS ON TODAY</h1>
                 <h1 className="heading_2">Join and share</h1>
-                <button className="login" onClick={()=>togglePop("signIn")}>Sign in</button>
-                {formToShow === "signIn" ? <SignInForm toggle={() => togglePop("signIn")}/> : null}
+                <button className="login" onClick={() => togglePop("signIn")}>Sign in</button>
+                {formToShow === "signIn" ? <SignInForm toggle={() => togglePop("signIn")} /> : null}
                 <button className="sign_up" onClick={togglePop}>Create account</button>
-                
+
             </div>
             <div className="logo">
                 <img src="logo.png" alt="logo" />
