bc3d40a3a4fd92d572e3fa0ab40ca4ac494cb86d
jlfernandes22 <95943327+jlfernandes22@users.noreply.github.com>
2024-11-15 12:28:35 +0000

    Login page css complete

--- Projeto_final/src/Login/SignInForm.jsx
+++ Projeto_final/src/Login/SignInForm.jsx
@@ -14,14 +14,14 @@ function SignInForm({ toggle }) {
     return (
         <form className='teste' onSubmit={handleLogin}>
             <div className="form">
-                <h1>Sign In</h1>
+                <h1 className='sign_in_heading'>Sign In</h1>
 
                 <label className='username_label'>Username</label>
-                <input type="text" placeholder="Enter your username" value={username} className="username" onChange={e => setUsername(e.target.value)} required
+                <input type="text" placeholder="   Enter your username" value={username} className="username" onChange={e => setUsername(e.target.value)} required
                 />
 
                 <label className='password_label'>Password</label>
-                <input type="password" placeholder="Enter your password" value={password} className="Password" onChange={e => setPassword(e.target.value)} required />
+                <input type="password" placeholder="   Enter your password" value={password} className="password" onChange={e => setPassword(e.target.value)} required />
 
                 <div className="Buttons">
                     <button onClick={toggle} className="cancel">Cancel</button>
