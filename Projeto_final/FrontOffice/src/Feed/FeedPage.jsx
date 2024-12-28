94208c4484ab868386c22786c001b16cdd682a80
MikoExtreme <160228078+MikoExtreme@users.noreply.github.com>
2024-12-28 23:47:36 +0000

    Create FeedPage.jsx

---
+++ Projeto_final/FrontOffice/src/Feed/FeedPage.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Feed from "./Feed";
+import Footer from "./Footer";
+
+function FeedPage(){
+    return(
+        <>
+            <Header></Header>
+            <Feed></Feed>
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default FeedPage;
