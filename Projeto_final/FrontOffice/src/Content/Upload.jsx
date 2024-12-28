7c6ba4b57b576caee01dd3dba36c6b7011fa4154
MikoExtreme <160228078+MikoExtreme@users.noreply.github.com>
2024-12-28 23:47:05 +0000

    Create Upload.jsx

---
+++ Projeto_final/FrontOffice/src/Content/Upload.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect, useRef} from "react";
+import "./cssfolderContent/upload.css"
+
+function Upload(){
+    return(
+        <div className="containerUpload">
+            ola
+        </div>
+    );
+}
+
+export default Upload;
